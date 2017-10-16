53cd1a96c3d710e38a31622aea6c3d81b0ed1edd
Abdullah Khan <abdullahkhan0503@gmail.com>
2017-10-16 12:10:07 -0600

    CSS file `.min` suffix
    added `.min` suffix to CSS file using `gulp-rename`. And, added stylesheet auto-prefixing using `gulp-autoprefixer`

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    prefix = require('gulp-autoprefixer');
 
-// task to compile sass files on save
+// task to compile sass files on save and output a minified CSS file
 gulp.task('sass', function() {
     return gulp.src('public/css/styles.sass')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename('styles.min.css'))
+        .pipe(prefix({browsers: ['last 2 versions']}))
         .pipe(gulp.dest('public/css/'));
 });
 
