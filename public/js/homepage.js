7d64dee253caddf9f5628bc32e6b2dc32bc15248
Abdullah Khan <abdullahkhan0503@gmail.com>
2017-10-23 22:36:38 -0600

    switch to arrow-function notation!
    switch most of the callbacks to arrow notation for readability.
    Also, add some condition checking for the "intro" paragraph; and
    based on that, control how the page gets displayed (regarding
    setTimeouts and stuff)

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -4,18 +4,18 @@ function updateScroll(){
 }
 
 const headline = document.getElementById("main-heading");
-setTimeout(function(){
+setTimeout(() => {
   headline.classList.remove("hide");
   headline.classList.add("fadeInDown", "animated");
   headline.classList.add("glitch");
   updateScroll();
 }, 1600);
 
-var getJSON = function(url, callback) {
+var getJSON = (url, callback) => {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", url, true);
   xhr.responseType = "json";
-  xhr.onload = function() {
+  xhr.onload = () => {
     let status = xhr.status;
     if (status === 200) {
       callback(null, xhr.response);
@@ -32,9 +32,9 @@ const clientInfoBrowser = document.getElementById("browser");
 const clientInfoSystem = document.getElementById("operating-system");
 const clientInfoPlace = document.getElementById("place");
 
-getJSON("https://ipinfo.io/json", function(err, ipInfo) {
+getJSON("https://ipinfo.io/json", (err, ipInfo) => {
   if (ipInfo && ipInfo != "" && ipInfo.ip != undefined && ipInfo.ip != "undefined") {
-    setTimeout(function() {
+    setTimeout(() => {
       introText.classList.remove("hide");
       introText.classList.add("fadeIn", "animated");
       clientInfoIP.innerHTML = ipInfo.ip;
@@ -53,19 +53,22 @@ getJSON("https://ipinfo.io/json", function(err, ipInfo) {
   }
 });
 
-const aboutMe = document.getElementById("about-me");
-setTimeout(function(){
-  aboutMe.classList.remove("hide");
-  aboutMe.classList.add("fadeIn", "animated");
-}, 15432);
-
 function parallaxScroll(){
   var parallax = document.querySelectorAll(".parallax"), speed = 0.5;
-  window.onscroll = function(){
-    [].slice.call(parallax).forEach(function(el,i){
+  window.onscroll = () => {
+    [].slice.call(parallax).forEach((el,i) => {
       var windowYOffset = window.pageYOffset,
           bgPos = "50% " + (windowYOffset * speed) + "px";
       el.style.backgroundPosition = bgPos;
     });
   };
 }
+
+setTimeout(() => {
+  //console.log(introText.classList.contains("hide"));
+  const aboutMe = document.getElementById("about-me");
+setTimeout(() => {
+  aboutMe.classList.remove("hide");
+  aboutMe.classList.add("fadeIn", "animated");
+}, (introText.classList.contains("hide") == false) ? 14000 : 500);
+}, 5000);
