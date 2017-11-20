2212b1c96fbbd3920c0dff18ab374ba2a1cdb821
Abdullah <abdullahkhan0503@gmail.com>
2017-11-20 18:14:31 +0000

    refactor: better documentation + semicolons!
    add comments to make for better code documentation and,
    add some missing semicolons.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,3 +1,4 @@
+// the base time for all the setTimeout functions
 let timeoutBase = 500;
 
 function updateScroll(){
@@ -5,9 +6,11 @@ function updateScroll(){
   intro.scrollTop = intro.scrollHeight;
 }
 
+// hide the footer and the links div in the beginning (for homepage only)
 document.getElementById("footer").classList.add("hide");
 document.getElementById("links").classList.add("hide");
 
+// apply animations to the homepage headline
 const headline = document.getElementById("main-heading");
 setTimeout(() => {
   headline.classList.remove("hide");
@@ -15,6 +18,7 @@ setTimeout(() => {
   updateScroll();
 }, timeoutBase + 1100);
 
+// XHR Prmoise function to GET json data 
 const getJSON = (url) => {
   return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
@@ -35,23 +39,28 @@ const getJSON = (url) => {
   });
 };
 
+// generic function to apply fade in animation to any element on the page 
 const fadeIn = element => {
   element.classList.remove("hide");
-  element.classList.add("fadeIn", "animated")
-}
+  element.classList.add("fadeIn", "animated");
+};
+
 
 // alternative: https://ipapi.co/json
 const getIpData = getJSON("https://ipinfo.io/json");
 
+// DOM variables for the intro paragraph only
 const introText = document.getElementById("intro-text"),
       clientInfoIP = document.getElementById("ip-address"),
       clientInfoBrowser = document.getElementById("browser"),
       clientInfoSystem = document.getElementById("operating-system"),
       clientInfoPlace = document.getElementById("place");
 
+// function to set the text of the elements defined above using 
+// the data from client.js and the JSON ip data 
 function showIntro(ipInfo) {
   setTimeout(() => {
-    fadeIn(introText)
+    fadeIn(introText);
     clientInfoIP.innerHTML = ipInfo.ip;
     const clientJS = new ClientJS();
     if (/Edge\/12./i.test(navigator.userAgent))
@@ -64,6 +73,8 @@ function showIntro(ipInfo) {
   }, timeoutBase + 4000);
 }
 
+// if getting the JSON was successful, show the intro paragraph
+// (if there was an error, keep the paragraph hidden and display a friendly console message)
 getIpData.then(ipInfo => {
   if(ipInfo.ip != undefined)
     showIntro(ipInfo);
@@ -72,6 +83,8 @@ getIpData.then(ipInfo => {
               ' - running error handler to hide intro text (and continue to display other content)');
 });
 
+// setTimeout to display the footer and the links div, which is why we hid it in the first place.
+// the timeout time is based on the intro paragraph being hidden or not. 
 setTimeout(() => {
   const aboutMe = document.getElementById("about-me"),
         footer = document.getElementById("footer"),
