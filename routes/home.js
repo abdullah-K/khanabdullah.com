e34d057ff0ea5c01928cda708919b8e79f9d3f93
Abdullah Khan <abdullahkhan0503@gmail.com>
2018-01-30 21:16:15 -0700

    implement "anonymous analytics" system (homepage only)
    add a counter for how many times the homepage has been requested and store
    the count in the analytics folder.

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,18 @@
 /* main homepage route */
 
 const express = require("express"),
-      router = express.Router();
+      router = express.Router(),
+      fs = require('fs');
 
+let homeCounter = 0;
 router.get("/", (request, response) => {
+  homeCounter++;
   response.render("home", {title: "Abdullah F. Khan",
                       author: "Abdullah F. Khan",
                       description: "Hello, I\'m Abdullah. Welcome to my home, on the internet."});
+  fs.writeFile("./analytics/homepage.txt", `${homeCounter} homepage visits so far`, function(err) {
+      (err) && console.log(err);
+  });
 });
 
 module.exports = router;
