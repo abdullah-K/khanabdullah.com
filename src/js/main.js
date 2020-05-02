84161008472a168c7755e79ec740aadd9c20e9e7
Abdullah Khan <contact@khanabdullah.com>
2020-05-02 00:46:13 -0600

    extract scroll icon + improve responsiveness
    Signed-off-by: Abdullah Khan <contact@khanabdullah.com>

--- src/js/main.js
+++ src/js/main.js
@@ -19,8 +19,8 @@ function scrollTo() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-   if(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-    let vh = window.innerHeight * 0.01
+  if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    let vh = window.matchMedia("(orientation: landscape)").matches ? window.innerHeight * 0.015 : window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
   }
   document.querySelector(".fallback-bg").classList.remove("fallback-bg")
@@ -29,5 +29,5 @@ document.addEventListener("DOMContentLoaded", () => {
     effect.setOptions({
       zoom: 0.7,
     })
-  }, 1000)
+  }, 100)
 })
