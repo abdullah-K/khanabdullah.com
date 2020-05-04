e8089071c7cbb87f28b31bf20cc72fafb9210cdd
Abdullah Khan <contact@khanabdullah.com>
2020-05-03 20:24:46 -0600

    progress on animations
    implement smooth scroll between anchors and page transitions + update styling
    
    Signed-off-by: Abdullah Khan <contact@khanabdullah.com>

--- src/js/main.js
+++ src/js/main.js
@@ -13,21 +13,63 @@ const effect = VANTA.WAVES({
   zoom: 1
 })
 
-function scrollTo() {
-  const links = document.querySelectorAll('.smooth-scroll')
-  links.forEach(each => (each.onclick = scrollAnchors))
-}
-
 document.addEventListener("DOMContentLoaded", () => {
+  console.log('%c Hey there! You can look at this website\'s source code on https://github.com/abdullah-K/khanabdullah.com', 'background: #1B498C; color: #fff;')
+
+  // section height adjustment for mobile
   if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
     let vh = window.matchMedia("(orientation: landscape)").matches ? window.innerHeight * 0.015 : window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
   }
+
+  // smooth scroll for anchor links
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', (e) => {
+      e.preventDefault()
+      document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      })
+    })
+  })
+
+  // remove fallback background to show waves background
   document.querySelector(".fallback-bg").classList.remove("fallback-bg")
-  scrollTo()
+
+  // zoom out animation for waves
   setTimeout(() => {
     effect.setOptions({
       zoom: 0.7,
     })
   }, 100)
+
+  let languageLinks = document.querySelectorAll(".language-link"),
+      languageSelector = document.querySelector(".language-selector")
+  if(languageLinks) {
+    languageLinks.forEach((link) => {
+      link.onclick = (e) => {
+        if(window.location.pathname.includes("fr")) {
+          languageSelector.classList.remove("french")
+          languageSelector.classList.add("english")
+        }
+        else {
+          languageSelector.classList.remove("english")
+          languageSelector.classList.add("french")
+        }
+
+        let body = document.querySelector("body")
+        e.preventDefault()
+        setTimeout(() => {
+          if(body.classList.contains("fade-out")) {
+            if(window.location.pathname.includes("fr")) {
+              window.location = "/"
+            }
+            else {
+              window.location = "/index-fr.html"
+            }
+          }
+        }, 800);
+        body.classList.add("fade-out")
+      }
+    })
+  }
 })
