d51f4543a62e53ec13d91bba74ab146f09fc478f
Abdullah Khan <contact@khanabdullah.com>
2021-01-07 19:16:37 -0700

    update github username references
    Signed-off-by: Abdullah Khan <contact@khanabdullah.com>

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # [khanabdullah.com](https://khanabdullah.com)
+
 [![Netlify Status](https://api.netlify.com/api/v1/badges/baa7cb77-fae5-4285-9055-dc0f157430bc/deploy-status)](https://app.netlify.com/sites/khanabdullah/deploys)
 
 ![khanabdullah.com preview](./design/preview.png)
@@ -6,18 +7,23 @@
 🌎 v2.0 of my personal website. Built with [Pug](https://github.com/pugjs/pug#pug), [SASS](https://sass-lang.com/), and [Gulp](https://gulpjs.com/). Deployed on [Netlify](https://www.netlify.com/).
 
 ## Installation
+
 To run the site locally:
-- `git clone https://github.com/abdullah-K/khanabdullah.com`
+
+- `git clone https://github.com/abdlkhn/khanabdullah.com`
 - `yarn install`
 - `yarn dev`
 
 To produce the dist/ folder without watching files, run:
+
 - `yarn prod`
 
 ## Version 1
-[khanabdullah.com v1.0](https://github.com/abdullah-K/khanabdullah.com/tree/v1)
+
+[khanabdullah.com v1.0](https://github.com/abdlkhn/khanabdullah.com/tree/v1)
 
 ## Thanks
+
 [Icons8](https://icons8.com)
 
 [Vanta.js](https://www.vantajs.com/)
--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,78 @@
 const effect = VANTA.WAVES({
-  el: ".waves",
-  mouseControls: true,
-  touchControls: true,
-  minHeight: 200.0,
-  minWidth: 200.0,
-  scale: 1.0,
-  scaleMobile: 1.0,
-  color: 0x0a2955,
-  shininess: 5.0,
-  waveHeight: 20,
-  waveSpeed: 0.4,
-  zoom: 1
-})
+	el: ".waves",
+	mouseControls: true,
+	touchControls: true,
+	minHeight: 200.0,
+	minWidth: 200.0,
+	scale: 1.0,
+	scaleMobile: 1.0,
+	color: 0x0a2955,
+	shininess: 5.0,
+	waveHeight: 20,
+	waveSpeed: 0.4,
+	zoom: 1,
+});
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log('%c Hey there! You can look at this website\'s source code on https://github.com/abdullah-K/khanabdullah.com', 'background: #1B498C; color: #fff;')
+	console.log(
+		"%c Hey there! You can look at this website's source code on https://github.com/abdlkhn/khanabdullah.com",
+		"background: #1B498C; color: #fff;"
+	);
 
-  // section height adjustment for mobile
-  if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-    let vh = window.matchMedia("(orientation: landscape)").matches ? window.innerHeight * 0.015 : window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
-  }
+	// section height adjustment for mobile
+	if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+		let vh = window.matchMedia("(orientation: landscape)").matches
+			? window.innerHeight * 0.015
+			: window.innerHeight * 0.01;
+		document.documentElement.style.setProperty("--vh", `${vh}px`);
+	}
 
-  // smooth scroll for anchor links
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener('click', (e) => {
-      e.preventDefault()
-      document.querySelector(anchor.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      })
-    })
-  })
+	// smooth scroll for anchor links
+	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+		anchor.addEventListener("click", e => {
+			e.preventDefault();
+			document.querySelector(anchor.getAttribute("href")).scrollIntoView({
+				behavior: "smooth",
+			});
+		});
+	});
 
-  // remove fallback background to show waves background
-  document.querySelector(".fallback-bg").classList.remove("fallback-bg")
+	// remove fallback background to show waves background
+	document.querySelector(".fallback-bg").classList.remove("fallback-bg");
 
-  // zoom out animation for waves
-  setTimeout(() => {
-    effect.setOptions({
-      zoom: 0.7,
-    })
-  }, 100)
+	// zoom out animation for waves
+	setTimeout(() => {
+		effect.setOptions({
+			zoom: 0.7,
+		});
+	}, 100);
 
-  let languageLinks = document.querySelectorAll(".language-link"),
-      languageSelector = document.querySelector(".language-selector")
-  if(languageLinks) {
-    languageLinks.forEach((link) => {
-      link.onclick = (e) => {
-        if(window.location.pathname.includes("fr")) {
-          languageSelector.classList.remove("french")
-          languageSelector.classList.add("english")
-        }
-        else {
-          languageSelector.classList.remove("english")
-          languageSelector.classList.add("french")
-        }
+	let languageLinks = document.querySelectorAll(".language-link"),
+		languageSelector = document.querySelector(".language-selector");
+	if (languageLinks) {
+		languageLinks.forEach(link => {
+			link.onclick = e => {
+				if (window.location.pathname.includes("fr")) {
+					languageSelector.classList.remove("french");
+					languageSelector.classList.add("english");
+				} else {
+					languageSelector.classList.remove("english");
+					languageSelector.classList.add("french");
+				}
 
-        let body = document.querySelector("body")
-        e.preventDefault()
-        setTimeout(() => {
-          if(body.classList.contains("fade-out")) {
-            if(window.location.pathname.includes("fr")) {
-              window.location = "/"
-            }
-            else {
-              window.location = "/fr"
-            }
-          }
-        }, 800);
-        body.classList.add("fade-out")
-      }
-    })
-  }
-})
+				let body = document.querySelector("body");
+				e.preventDefault();
+				setTimeout(() => {
+					if (body.classList.contains("fade-out")) {
+						if (window.location.pathname.includes("fr")) {
+							window.location = "/";
+						} else {
+							window.location = "/fr";
+						}
+					}
+				}, 800);
+				body.classList.add("fade-out");
+			};
+		});
+	}
+});
